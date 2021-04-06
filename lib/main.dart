91a65fc4c9c6eab3c30ba3e84f01e45b6113da9c
ronnyerybarbosa <ronnyerybarbosa@gmail.com>
2021-04-06 00:35:29 -0300

    set state

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,39 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  var resultado = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Meu contador"),
       ),
-      body: Container(
-        color: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            'Você pressinou o botão',
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          )),
+          Center(
+            child: Text(
+              '$resultado',
+              style: TextStyle(fontSize: 34),
+            ),
+          ),
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Botão pressionado');
+            setState(() {
+              resultado += 1;
+            });
+          },
+          child: Icon(Icons.add)),
     );
   }
 }
